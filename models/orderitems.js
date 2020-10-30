4a8c2d2360076bf95f9b2a20f99cbb78cdc43074
oliverclaxton <oliver.claxton66@gmail.com>
2020-10-30 13:46:11 +0100

    added orderItems table

---
+++ models/orderitems.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class orderItems extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  orderItems.init({
+    userId: DataTypes.INTEGER,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'orderItems',
+  });
+  return orderItems;
+};
